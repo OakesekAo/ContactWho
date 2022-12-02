ff3a6d778b58b11be9c2ec92ef0741f95a3235e5
OakeseskAo <oakes.a@live.com>
2022-12-01 22:57:15 -0500

    Updated AppUser
    Added part of template and setup auth

---
+++ Models/Contact.cs
@@ -0,0 +1,65 @@
+﻿
+using ContactPro.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactPro.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? AppUserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+        [Display(Name ="Birthday")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public States State { get; set; }
+
+        [Required]
+        [Display(Name ="Zip Code")]
+        [DataType(DataType.PostalCode)]
+        public int ZipCode { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+
+        //image properties
+        public byte[]? ImageDate { get; set; }
+        public string? ImageType { get; set; }
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+
+        //virtuals
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+    }
+}

d7423eaa25232487752bb39dbc5536e2331790df
OakeseskAo <oakes.a@live.com>
2022-12-10 09:49:38 -0500

    Styled Categories Index
    Added some styling and hooked up proper paths

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using ContactPro.Data;
 using ContactPro.Models;
+using ContactPro.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 
 namespace ContactPro.Controllers
@@ -16,18 +17,26 @@ namespace ContactPro.Controllers
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public CategoriesController(ApplicationDbContext context)
+        public CategoriesController(ApplicationDbContext context, 
+                                    UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Categories
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Categories.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+            string appUserId = _userManager.GetUserId(User);
+
+
+            var categories = await _context.Categories.Where(c => c.AppUserId == appUserId)
+                                                .Include(c => c.AppUser)
+                                                .ToListAsync();
+            return View(categories);
         }
 
         // GET: Categories/Details/5

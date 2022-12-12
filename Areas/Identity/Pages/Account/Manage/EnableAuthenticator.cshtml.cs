9ab10cbc72b848c24cc07f867b627e571bd2efac
OakeseskAo <oakes.a@live.com>
2022-12-12 01:01:03 -0500

    Added 2FA QR code render
    Added 2FA QR code render

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace ContactPro.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Contact Pro"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
